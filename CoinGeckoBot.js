ed1016dc85226dfbf9768ed8eae7d2cc280c46aa
EliasVincent <riesyeti@outlook.de>
2021-01-27 16:51:46 +0100

    first commit

---
+++ CoinGeckoBot.js
@@ -0,0 +1,63 @@
+const Mastodon = require("mastodon-api");
+const fetch = require("node-fetch");
+const cron = require("node-cron");
+const express = require("express");
+
+console.log("Starting bot..");
+
+// create new Mastodon Bot
+const Bot = new Mastodon({
+  client_id: "insert here",
+  client_secret: "insert here",
+  access_token: "insert here",
+  timeout_ms: 60 * 1000,
+  api_url: "https://catspace.xyz/api/v1/",
+});
+
+let btcPrice = "";
+
+// fetch API data
+async function fetchData() {
+  const response = await fetch(
+    "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+  );
+
+  if (response.ok) {
+    const responseData = await response.json(); // jsonifys the response we get
+    console.log(responseData);
+    btcPrice = responseData.bitcoin.usd;
+    console.log(btcPrice);
+
+    postToot();
+  } else {
+    console.log("API Error");
+  }
+}
+
+// create the post
+function postToot() {
+  const params = {
+    status: `Bitcoin price right now: ${btcPrice}$`,
+  };
+
+  Bot.post("statuses", params, (error, data) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+
+// create express backend server 
+app = express();
+
+// it now runs once every full hour
+cron.schedule('0 * * * *', function() {
+    console.log('posting scheduled toot..');
+    fetchData()
+    console.log('done!');
+  });
+
+app.listen(3030);
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Mastodon CoinGecko Bot
+
+A bot for Mastodon that fetches data from the public CoinGecko API, made with Node
+
+> Public API means: no API keys needed!
+
+# Setup
+
+- Go to your Mastodon instance and create a new application under *Preferences -> Development*
+- Insert client_id, client_secret and access_token inside of `CoinGeckoBot.js`
+- Visit [CoinGecko's API Documentation](https://www.coingecko.com/en/api#explore-api) on how to use the API
+
+# Install
+
+- Install NodeJS on your computer/server
+- Run `npm install` in the root directory of the project
+- Start the bot with `node ./CoinGeckoBot.js`
+
+# Run Bot in the Background
+
+**Forever** is a great tool that lets you run Node apps in the background
+
+- install forever globally with `sudo npm install -g forever` *you might need to do this with sudo*
+- start bot in the background with `forever start -a CoinGeckoBot.js`
+- check all running apps with `forever list`
+- stop bot with `forever stop CoinGeckoBot.js`
+
+[see the bot in action](https://catspace.xyz/web/accounts/805)
+
+*(yes there is an npm package for the CoinGecko API, but with this bot it's not dependent on yet another package)*
